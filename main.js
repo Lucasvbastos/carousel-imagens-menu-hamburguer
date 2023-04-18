7d8b1c890fea889f8216e949f2495951d6affcd3
Lucas Viana Bastos <117181689+Lucasvbastos@users.noreply.github.com>
2023-04-17 21:36:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $('#carousel-images').slick({
         autoplay: true,
     });
+
+    $('.menu-hamburguer').click(function() {
+        $('nav').slideToggle();
+    });
 })
